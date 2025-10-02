1a80d2d5eb2c0589947649c81a89d9c338936ead
Goober <tymek.kowalewski2008@gmail.com>
2025-10-02 12:02:26 +0200

    Made couple of changes like displaying data as cards, adding more cards(data)

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,13 @@ import './App.css'
 import CoffeeCard from './CoffeeCard.jsx'
 function App() {
   const coffeList =[
-    {id: 1, name: "test", type: "none", description: "", isNew: true},
+    {id: 1, name: "test", type: "none", description: "yo", isNew: true},
+    {id: 2, name: "Cappucishish", type: "I dunno", description: "I don't know how its spelled", isNew: false},
+    {id: 3, name: "test", type: "none", description: "yo", isNew: false},
+    {id: 4, name: "test", type: "none", description: "yo", isNew: false},
+    {id: 5, name: "test", type: "none", description: "yo", isNew: false},
+    {id: 6, name: "test", type: "none", description: "yo", isNew: false},
+    
   ]
 
   
--- src/CoffeeCard.jsx
+++ src/CoffeeCard.jsx
@@ -5,9 +5,9 @@ function CoffeeCard({id,name,type,desc,isNew}){
     return(
         <div className='card' key={id}>
           <h2>{name}</h2>
-          <h3>{type}</h3>
+          <h3>Type: {type}</h3>
           <p>{desc}</p>
-          <p>{isNew ? "Nowość!" : ""}</p>
+          <p><b>{isNew ? "Nowość!" : ""}</b></p>
         </div>
     )
 }
